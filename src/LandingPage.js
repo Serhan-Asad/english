a0268f112407eae2cb82f06ff4a5f92aa377c04c
Serhan Asad <serhanasad2013@live.com>
2023-04-12 23:13:53 -0400

    second

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -13,7 +13,7 @@ function LandingPage() {
   return (
     <div className="App">
       <Router>
-      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar bg="dark" variant="dark" expand="lg">
           <Container>
             <Navbar.Brand href="/home">The Mindful Decider</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
@@ -28,34 +28,35 @@ function LandingPage() {
         </Navbar>
         <Container>
           <Routes>
-            <Route path="/discovering-yourself" element={<DiscoveringYourself />} />
-            <Route path="/exploring-psychology" element={<PsychologyOfDecisionMaking />} />
-            <Route path="/Home" element={<Home/>}/>
-            <Route path="/works-cited" element={<WorksCited/>}/>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/discovering-yourself" element={<DiscoveringYourself />} />
+            <Route exact path="/exploring-psychology" element={<PsychologyOfDecisionMaking />} />
+            <Route exact path="/works-cited" element={<WorksCited />} />
+            <Route exact path="/home" element={<Home />} />
+
           </Routes>
         </Container>
         <footer>
-        <Container>
-          <Row>
-            <Col className="text-center py-3">
-              <p>© 2023 The Mindful Decider. All rights reserved.</p>
-              <div className="social-icons">
-                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                  <FaFacebookF />
-                </a>
-                <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-                  <FaTwitter />
-                </a>
-                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-                  <FaInstagram />
-                </a>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </footer>
-
-       </Router>
+          <Container>
+            <Row>
+              <Col className="text-center py-3">
+                <p>© 2023 The Mindful Decider. All rights reserved.</p>
+                <div className="social-icons">
+                  <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                    <FaFacebookF />
+                  </a>
+                  <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                    <FaTwitter />
+                  </a>
+                  <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram />
+                  </a>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </footer>
+      </Router>
     </div>
   );
 }
